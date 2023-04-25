c3478f079af287a654042e620b62616ef8722902
Adiba <adibasadaf300@gmail.com>
2023-04-25 11:23:31 +0530

    Initil commit

---
+++ color.py
@@ -0,0 +1,45 @@
+
+
+
+bg_color ={
+    2 : "#eee4da",
+    4 : '#ede0c8',
+    8: '#edc850',
+    16: '#edc53f',
+    32: '#f67c5f',
+    64: '#f65e3b',
+    128: '#edcf72',
+    256: '#edcc61',
+    512: '#f2b179',
+    1024: '#f59563',
+    2048: '#edc22e',
+}
+fg_color ={
+    2 : '#776e65',
+    4: '#f9f6f2',
+    8: '#f9f6f2',
+    16 : '#f9f6f2',
+    32 : '#f9f6f2',
+    64 : '#f9f6f2',
+    128: '#f9f6f2',
+    256: '#f9f6f2',
+    512: '#776e65',
+    1024: '#f9f6f2',
+    2048: '#f9f6f2',
+}
+
+color_fonts = {
+     2 : ("Helvetica",40, "bold") ,
+      4 : ("Helvetica",40, "bold"),
+    8: ("Helvetica",40, "bold"),
+    16: ("Helvetica",35, "bold"),
+    32:("Helvetica",35, "bold"),
+    64: ("Helvetica",35, "bold"),
+    128: ("Helvetica",30, "bold"),
+    256: ("Helvetica",30, "bold"),
+    512: ("Helvetica",30, "bold"),
+    1024: ("Helvetica",25, "bold"),
+    2048: ("Helvetica",25, "bold"),
+
+}
+print(bg_color[2])
---
+++ workit.py
@@ -0,0 +1,234 @@
+import tkinter as tk
+import color as clr
+import random
+
+class Game:
+    def __init__(self):
+        self.window=tk.Tk()
+        self.window.geometry("600x550")
+        self.window.resizable(height=False, width=False)
+        self.window.config(bg="black")
+        self.window.title("2048 game")
+        self.game_area=tk.Frame(self.window,bg="#AD6260",bd=10,borderwidth=10,height=500,width=500)
+        self.game_area.grid(padx=80,pady=80)
+        self.make_GUI()
+        self.initialize()
+        self.window.bind("<Left>",self.left)
+        self.window.bind("<Right>", self.right)
+        self.window.bind("<Up>", self.up)
+        self.window.bind("<Down>", self.down)
+
+
+        self.window.mainloop()
+
+    #making empty 4×4 cell
+    def make_GUI(self):
+        self.cell=[]
+
+        for i in range(4):
+            row=[]
+            for j in range(4):
+                cell_frame = tk.Frame(self.game_area,bg="#FAABF1",height=100,width=100)
+                cell_number = tk.Label(self.game_area,bg="#FAABF1")
+                cell_frame.grid(row=i,column=j,pady=5,padx=5)
+                cell_number.grid(row=i,column=j)
+                cell_data={"frame":cell_frame,"number":cell_number}
+                row.append(cell_data)
+            self.cell.append(row)
+
+        score_frame = tk.Frame(self.window, height=70,width=150,bg="#B06362")
+        score_frame.place(x=200,y=5,height=60,width=200)
+        tk.Label(score_frame, text="SCORE",font=("ariel",14,"bold"),bg="#B06362").place(x=50,y=0)
+        self.score_label = tk.Label(score_frame, text="0",font=("ariel",14,"bold"),bg="#B06362")
+        self.score_label.place(x=80,y=28)
+
+
+    #initialize the game by randomly placing the initial two 2s at ramdom places
+    def initialize(self):
+        #creating matrix of zeroes
+        self.matrix = [[0]*4 for _ in range(4) ]  #empty matrix
+
+        r = random.randint(0,3)
+        c = random.randint(0,3)
+
+        self.matrix[r][c]=2
+        self.cell[r][c]["frame"].config( bg = clr.bg_color[2]),
+        self.cell[r][c]["number"].config( bg = clr.bg_color[2],
+            fg = clr.fg_color[2],
+            font= clr.color_fonts[2],
+            text="2"
+        )
+        #the second 2,making sure it does not go at the previous place
+
+        while(self.matrix[r][c] != 0):
+
+            r = random.randint(0, 3)
+            c = random.randint(0, 3)
+        self.matrix[r][c] = 2
+        self.cell[r][c]["frame"].config(bg=clr.bg_color[2]),
+        self.cell[r][c]["number"].config(bg=clr.bg_color[2],
+                                         fg=clr.fg_color[2],
+                                         font=clr.color_fonts[2],
+                                         text="2"
+                                         )
+        self.score = 0
+
+    #we will create few function now,b using then we can manipulate each move into left move
+    #func to stack each move to the lest side
+    def stack(self):
+        new_mat = [[0]*4 for _ in range(4) ]
+        for i in range(4):    #range 4 is from 0 to 3
+            no_of_position = 0
+            for j in range(4):
+                if(self.matrix[i][j] != 0):
+                    new_mat[i][no_of_position] = self.matrix[i][j]
+                    no_of_position += 1
+        self.matrix = new_mat
+        print("stack done")
+
+    #func to reverse the matrix
+    def reverse(self):
+        new_mat = [[0] * 4 for _ in range(4)]
+        for i in range(4):
+            for j in range(4):
+                new_mat[i][j] = self.matrix[i][3-j]
+        self.matrix = new_mat
+        print("reverse done")
+
+
+    #func to transpose the matrix
+    def transpose(self):
+        new_mat = [[0] * 4 for _ in range(4)]
+        for i in range(4):
+            for j in range(4):
+                new_mat[i][j] = self.matrix[j][i]
+        self.matrix = new_mat
+        print("transpose")
+
+    #fucn to combine two value that are same and next to each other(only for a left move)
+    def combine(self):
+        for i in range(4):
+            for j in range(3):
+                if self.matrix[i][j] !=0 and self.matrix[i][j] == self.matrix[i][j+1]:
+                    self.matrix[i][j] *= 2
+                    self.matrix[i][j+1] = 0
+                    self.score += self.matrix[i][j]
+        print("combine")
+
+
+    #add 2 or 4 ramdomly to an empt cell
+    def add_newtile(self):
+        if any(0 in row for row in self.matrix):
+            r = random.randint(0, 3)
+            while (self.matrix[r][3] != 0):
+                r = random.randint(0, 3)
+            self.matrix[r][3] = random.choice([2, 4])
+
+
+    # update the GUI to a new row or column (from the left side;) )
+    def update_GUI(self):
+        for i in range(4):
+            for j in range(4):
+                cell_value = self.matrix[i][j]
+                if cell_value == 0:
+                    self.cell[i][j]["frame"].config(bg="#FAABF1"),
+                    self.cell[i][j]["number"].config(bg="#FAABF1",text="")
+                else:
+                    self.cell[i][j]["frame"].config(bg=clr.bg_color[cell_value])
+                    self.cell[i][j]["number"].config(bg=clr.bg_color[cell_value],
+                                                     fg=clr.fg_color[cell_value],
+                                                     font=clr.color_fonts[cell_value],
+                                                     text=str(cell_value))
+        self.score_label.config(text=self.score)
+        self.window.update_idletasks()
+        for i in range(4):
+            for j in range (4):
+                print("matrix now is:", self.matrix[i][j])
+            print("\n")
+    #four func for four possible moves
+    def left(self, event):
+        self.stack()
+        self.combine()
+        self.stack()
+        self.add_newtile()
+
+        self.update_GUI()
+        self.game_status()
+
+    def right(self, event):
+        self.reverse()
+        self.stack()
+        self.combine()
+        self.stack()
+        self.add_newtile()
+        self.reverse()
+
+        self.update_GUI()
+        self.game_status()
+
+    def up(self, event):
+        self.transpose()
+        self.stack()
+        self.combine()
+        self.stack()
+        self.add_newtile()
+        self.transpose()
+
+        self.update_GUI()
+        self.game_status()
+
+    def down(self, event):
+        self.transpose()
+        self.reverse()
+        self.stack()
+        self.combine()
+        self.stack()
+        self.add_newtile()
+        self.reverse()
+        self.transpose()
+
+        self.update_GUI()
+        self.game_status()
+
+    #func to check if any possible moves left
+    def horizontal_moves_exits(self):
+        for i in range(4):
+            for j in range(3):
+                if(self.matrix[i][j] ==self.matrix[i][j+1]):
+                    return True
+
+        return False
+
+    def vertical_moves_exits(self):
+        for i in range(3):
+            for j in range(4):
+                if(self.matrix[i][j]==self.matrix[i+1][j]):
+                    return True
+
+        return False
+
+    #to check if the game is over (win\lose)
+    def game_status(self):
+        #game won
+        if any(2048 in row for row in self.matrix):
+            game_status_frame = tk.Frame(self.game_area, borderwidth=2)
+            game_status_frame.place(relx=0.5,rely=0.5, anchor="centre")
+            tk.Label(game_status_frame, text="YOU WON!!!",bg="#FFCC00",
+                     fg="#26FFE1",
+                     font=("Helvetica",50, "bold"),
+                      ).pack()
+
+        elif not any( 0 in row for row in self.matrix) and not self.horizontal_moves_exits() and not self.vertical_moves_exits():
+            game_status_frame = tk.Frame(self.game_area, borderwidth=2)
+            game_status_frame.place(relx=0.5, rely=0.5, anchor="centre")
+            tk.Label(game_status_frame, text="YOU LOSE!!!",
+                     bg="#a39489",
+                     fg ="#26FFE1",
+                    font =("Helvetica",50, "bold"),
+            ).pack()
+
+def main():
+    Game()
+
+if __name__ == "__main__" :
+    main()
